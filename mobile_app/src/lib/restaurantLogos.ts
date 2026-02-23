2955a095d4cbb484be907bce8145d8631c0a32fe
UsoguiH <181644116+UsoguiH@users.noreply.github.com>
2026-02-23 03:22:06 +0300

    feat: Add white background to logos and link remaining 8 restaurant logos

--- mobile_app/src/lib/restaurantLogos.ts
+++ mobile_app/src/lib/restaurantLogos.ts
@@ -4,6 +4,14 @@ export const restaurantLogos: Record<string, any> = {
     'الطازج': require('../../assets/logos/altazaj.png'),
     'باسكن روبنز': require('../../assets/logos/baskin_robbins.png'),
     'هرفي': require('../../assets/logos/herfy.png'),
+    'ماكدونالدز': require('../../assets/logos/mcdonalds.png'),
+    'شاورمر': require('../../assets/logos/shawarmer.png'),
+    'كودو': require('../../assets/logos/kudu.png'),
+    'بيتزا هت': require('../../assets/logos/pizzahut.png'),
+    'ماما نورة': require('../../assets/logos/mamanoura.png'),
+    'كنتاكي': require('../../assets/logos/kfc.png'),
+    'صب واي': require('../../assets/logos/subway.png'),
+    'ستاربكس': require('../../assets/logos/starbucks.png'),
 };
 
 export const getRestaurantLogo = (restaurantNameAr: string): any | null => {
